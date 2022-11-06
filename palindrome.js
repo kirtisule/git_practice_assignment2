b685abc1efcb1331978a4c6ea84d0cb89947f23f
keshav0605 <keshav06patel@gmail.com>
2022-11-06 22:03:53 +0530

    code fixed by keshav

--- palindrome.js
+++ palindrome.js
@@ -3,7 +3,7 @@ let rev = "";
 for(let i=str.length-1;i<=0;i--){
 rev = rev + str[i];
 }
-if (rev=str){
+if (rev==str){
   console.log("yes");
 else{
   console.log("no");
--- prime.js
+++ prime.js
@@ -4,3 +4,8 @@ for(let i=1;i<=num; i++){
 if(num%i==0){
 factor++
 }
+if(factor==2){
+console.log("True");
+}else{
+console.log("False");
+}
